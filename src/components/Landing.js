8f9908d11a31094569c02167b1a5388f47fec15b
Stephie <stephaniecrocker91@gmail.com>
2023-09-17 13:54:11 +0000

    Initial setup

---
+++ src/components/Landing.js
@@ -0,0 +1,27 @@
+import {React} from 'react';
+import styles from '../styles/Landing.module.css'
+import image from '../images/starimage.png'
+import Button from 'react-bootstrap/Button';
+import { NavLink } from 'react-router-dom'
+import Video from './Video';
+
+
+function Landing() {
+    return (
+        <>
+                <img src={image} className={styles.Mobile}/>
+                <Video />
+                <div className={
+                    styles.LandingText
+                }>
+                    <h1>Richard Morales</h1>
+                    <h2>terapeuta holístico</h2>
+                </div>
+                <NavLink to="/registrosakashicos">
+                    <Button variant="dark" className={styles.Button}>Bienvenidos</Button>
+                </NavLink>
+        </>
+    );
+}
+
+export default Landing;
